e267b46ae89c62aa2a939b0b9b7e2084a996ca50
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2025-09-05 19:34:57 +0530

    Production grade components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import { Header, Footer } from "./components"
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  // console.log(process.env.REACT_APP_APPWRITE); this is for Creat React App below is for vite
-  // console.log(import.meta.env.VITE_APP_APPWRITE); // but there is more preferred way with conf file    
-  return (
-    <>
-      <h1>blog with appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [dispatch])
+
+  return !loading ? (
+    <div className='w-full min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
---
+++ src/components/Input.jsx
@@ -0,0 +1,29 @@
+import React, { useId } from 'react'
+
+const Input = React.forwardRef(function Input({
+    label,
+    type = 'text',
+    className = '',
+    ...props
+}, ref) {
+    const id = useId();
+    return (
+        <div className="w-full">
+            {label && 
+                <label 
+                    className='inline-block mb-1 pl--1' 
+                    htmlFor={id}>
+                    {label}
+                </label>}
+            <input 
+                id={id}
+                type={type}
+                className={`w-full px-4  ${className}`}
+                ref={ref}
+                {...props}  
+            />
+        </div>
+    )
+})
+
+export default Input
